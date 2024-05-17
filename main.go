08ed2e641d55ed32aca85f0965a8dc9a371c8d09
Ashik Salahudeen <ashik.salahudeen@kreuzwerker.de>
2024-05-17 12:25:31 +0200

    add some control flow and loop demos

--- main.go
+++ main.go
@@ -31,18 +31,24 @@ func main() {
 	fmt.Println(greeting())
 	fmt.Println(squareIt(17))
 
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Printf("Hello world for the %d-th time\n", i)
+		}
+	}
+
 	fmt.Printf("Greeting: %s\n", greeting())
 
-	var square Square = Square{side: 1.0}
-	fmt.Printf("Area of Square %f\n", square.Area()) // Direct function call
+	//var square Square = Square{side: 1.0}
+	//fmt.Printf("Area of Square %f\n", square.Area()) // Direct function call
 
-	var circle Circle = Circle{radius: 1.0}
-	fmt.Printf("Area of Circle %f\n", circle.Area()) // Direct function call
+	//var circle Circle = Circle{radius: 1.0}
+	//fmt.Printf("Area of Circle %f\n", circle.Area()) // Direct function call
 
-	var shape1 Shape = Square{side: 2.0} // Huh ? YEAH.
-	//var shape1 Shape = Circle{radius: 5.0}
-	fmt.Printf("Area of shape %f\n", shape1.Area())
+	// var shape1 Shape = Square{side: 2.0} // Huh ? YEAH.
+	// var shape1 Shape = Circle{radius: 5.0}
+	// fmt.Printf("Area of shape %f\n", shape1.Area())
 
 	// It can also do run time type checking !!
-	demoRTTI(shape1)
+	// demoRTTI(shape1)
 }
